c08c10b976dd989a1bbcbb3c00ed2ce74d5d8699
valik94 <valentin.lyashenko@ryerson.ca>
2021-11-06 18:46:07 +0000

    adding new fetcher.js function that takes in URL and a file path and downloads the website HTML body and response, and displays the amount of bytes downloaded by using 1-1 ratio of characters to bytes. Could be improved upon by edge cases.

---
+++ fetcher.js
@@ -0,0 +1,38 @@
+const request = require('request');
+const fs = require('fs')
+
+//const content = 'Some content!'
+
+request('http://www.example.edu/', (err, response, body) => {
+  
+fs.writeFile('./index.html', body, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    //console.log(body);
+    //file written successfully
+  })
+fs.appendFile('./index.html', body, err => {
+  let count;
+  if (err) {
+    console.error(err)
+    return
+  }
+    count = body.split("").length;
+    console.log(`Downloaded and saved ${count} bytes to ./index.html`);
+})
+
+// fs.readFile('/index.html', 'utf8' , (err, data) => {
+//   //let count =0;
+//   if (err) {
+//     console.error(err)
+//     return
+//   }
+//   // count = data.split("").length;
+//   // console.log(count);
+// })
+
+});
+// INPUT: > node fetcher.js http://www.example.edu/ ./index.html
+// OUTPUT: Downloaded and saved 3261 bytes to ./index.html
